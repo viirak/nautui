85b346110ecd97fe7b6ccaad607c4c2c6e8167e5
viirak <viirak@gmail.com>
2026-05-04 13:37:36 +0700

    feat: Add Group component with flex layout utilities

--- packages/core/src/index.d.ts
+++ packages/core/src/index.d.ts
@@ -1,6 +1,7 @@
 import type { BoxProps } from "./components/Box.astro";
 import type { ButtonProps } from "./components/Button.astro";
 import type { ContainerProps } from "./components/Container.astro";
+import type { GroupProps } from "./components/Group.astro";
 import type { SectionProps } from "./components/Section.astro";
 import type { ThemeProps } from "./components/Theme.astro";
 import type { ThemeToggleProps } from "./components/ThemeToggle.astro";
@@ -15,6 +16,7 @@ export declare const ThemeToggle: (props: ThemeToggleProps) => AstroComponent;
 export declare const Container: (props: ContainerProps) => AstroComponent;
 export declare const Section: (props: SectionProps) => AstroComponent;
 export declare const Box: (props: BoxProps) => AstroComponent;
+export declare const Group: (props: GroupProps) => AstroComponent;
 
 // Elements
 export declare const Button: (props: ButtonProps) => AstroComponent;
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,8 +3,9 @@
 import Box from "./components/Box.astro";
 import Button from "./components/Button.astro";
 import Container from "./components/Container.astro";
+import Group from "./components/Group.astro";
 import Section from "./components/Section.astro";
 import Theme from "./components/Theme.astro";
 import ThemeToggle from "./components/ThemeToggle.astro";
 
-export { Box, Button, Container, Section, Theme, ThemeToggle };
+export { Box, Button, Container, Group, Section, Theme, ThemeToggle };
