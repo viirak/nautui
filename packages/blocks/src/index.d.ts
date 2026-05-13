60665b4039890862db95c4657c00c11082bd05ef
viirak <viirak@gmail.com>
2026-05-14 00:06:11 +0700

    feat: remove BackgroundGradient component and related exports

--- packages/blocks/src/index.d.ts
+++ packages/blocks/src/index.d.ts
@@ -1,12 +1,6 @@
-import type { BackgroundGradientProps } from "./components/backgrounds/BackgroundGradient.astro";
 import type { SectionHeroProps } from "./components/SectionHero.astro";
 
 export type AstroComponent = () => Promise<{ Content: unknown }>;
-
-// Background
-export declare const BackgroundGradient: (
-  props: BackgroundGradientProps
-) => AstroComponent;
 
 // UI
 export declare const SectionHero: (props: SectionHeroProps) => AstroComponent;
--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -1,6 +1,5 @@
 // biome-ignore-all lint/style/noExportedImports: DX
 
-import BackgroundGradient from "./components/backgrounds/BackgroundGradient.astro";
 import SectionHero from "./components/SectionHero.astro";
 
-export { BackgroundGradient, SectionHero };
+export { SectionHero };
