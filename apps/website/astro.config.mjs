75d0d55e0a281d8cd5c3f242247ffaa0a51e0498
viirak <viirak@gmail.com>
2026-07-27 16:39:02 +0700

    feat: Add Cloudflare adapter to website
    Add Cloudflare adapter configuration and dependencies for deployment.

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,11 +2,16 @@
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   integrations: [mdx()],
+
   vite: {
     css: {
       transformer: "lightningcss",
     },
   },
+
+  adapter: cloudflare(),
 });
