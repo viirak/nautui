a14c27e2ac91a2442b0944e970e77b80f65411d1
viirak <viirak@gmail.com>
2026-08-21 18:41:05 +0700

    fix: changes in packages/blocks/src/index.d.ts, packages/blocks/src/index.ts

--- packages/blocks/src/index.d.ts
+++ packages/blocks/src/index.d.ts
@@ -1,5 +1,6 @@
 import type { BreadcrumbProps } from "./components/Breadcrumb.astro";
 import type { DocLayoutProps } from "./components/DocLayout.astro";
+import type { NavMenuProps } from "./components/NavMenu.astro";
 import type { SectionHeroProps } from "./components/SectionHero.astro";
 import type { TOCProps } from "./components/TOC.astro";
 
@@ -10,5 +11,6 @@ export declare const DocLayout: (props: DocLayoutProps) => AstroComponent;
 
 // UI
 export declare const Breadcrumb: (props: BreadcrumbProps) => AstroComponent;
+export declare const NavMenu: (props: NavMenuProps) => AstroComponent;
 export declare const SectionHero: (props: SectionHeroProps) => AstroComponent;
 export declare const TOC: (props: TOCProps) => AstroComponent;
--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -2,7 +2,8 @@
 
 import Breadcrumb from "./components/Breadcrumb.astro";
 import DocLayout from "./components/DocLayout.astro";
+import NavMenu from "./components/NavMenu.astro";
 import SectionHero from "./components/SectionHero.astro";
 import TOC from "./components/TOC.astro";
 
-export { Breadcrumb, DocLayout, SectionHero, TOC };
+export { Breadcrumb, DocLayout, NavMenu, SectionHero, TOC };
